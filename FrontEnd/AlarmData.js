c85bdd5255994ae27535875c25557e7f88c3ba35
Alexander Chu <arctangent1759@ubuntu.(none)>
2013-03-09 04:36:28 -0800

    fixed displays

--- FrontEnd/AlarmData.js
+++ FrontEnd/AlarmData.js
@@ -1,5 +1,5 @@
 var date = new Date("2013-03-09T12:27:00.000Z");
 $(".name").html("Title")
 $(".description").html("Insert embedded ruby here that will insert the description of the alarm i don't know what i'm saying it's late.")
-$(".alarmtime").html("02:04:30")
+$(".alarmtime").html((function (x){return x.getHours()+":"+x.getMinutes()+":"+x.getSeconds()+" on "+x.getDay()+"/"+x.getMonth()+"/"+x.getFullYear()})(date))
 $(".checkins").html("5")
